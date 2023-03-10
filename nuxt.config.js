d5eef97ce441bc9c68d69e50a7cb78cd62313194
FilCom <filippo@comunicativi.it>
2023-03-10 14:57:33 +0100

    Meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
 	target: 'static',
 
 	head: {
-		title: 'Q Italy Vantaggi Fiscali 2022',
+		title: 'Vantaggi fiscali acquisto macchinari centri estetici',
 		htmlAttrs: {
 			lang: 'it'
 		},
@@ -12,9 +12,9 @@ export default {
 			{ hid: 'description', name: 'description', content: '' },
 			{ name: 'format-detection', content: 'telephone=no' },
 			{ name: 'og:type', content: 'website'},
-            { hid: 'description', name: 'description', content: 'Fino all\'80% di agevolazioni fiscali con credito d’imposta e industria 4.0. Scopri i nostri prodotti e approfitta subito degli incentivi' },
+            { hid: 'description', name: 'description', content: 'Scopri quali sono i vantaggi fiscali dedicati ai centri estetici e all\'acquisto di nuove apparecchiature.' },
             { hid: 'og:title', name: 'og:title', content: 'Q Italy Vantaggi Fiscali 2022'},
-            { hid: 'og:description', name: 'og:description', content: 'Fino all\'80% di agevolazioni fiscali con credito d’imposta e industria 4.0. Scopri i nostri prodotti e approfitta subito degli incentivi'},
+            { hid: 'og:description', name: 'og:description', content: 'Scopri quali sono i vantaggi fiscali dedicati ai centri estetici e all\'acquisto di nuove apparecchiature.'},
             { name: 'og:url', content: 'https://q-italy.it/vantaggi-fiscali-2022/'},
             //{ name: 'og:image', content: 'TODO'}
 		],
